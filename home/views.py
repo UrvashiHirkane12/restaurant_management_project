fdc7e85671871f02e3f7974ff3894bb356576be7
UrvashiHirkane12 <168714099+UrvashiHirkane12@users.noreply.github.com>
2025-08-22 09:44:53 +0530

    36840

--- home/views.py
+++ home/views.py
@@ -6,4 +6,11 @@ def home(request):
     }
     return render(request, "home.html", context)
 
+def about(request):
+    context = {
+        "restaurant_name": settings.RESTAURANT_NAME,
+        "description": "we are a renowed restaurant",
+
+    } 
+   return render(request, "about.html", context) 
 # Create your views here.
