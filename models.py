8f8e12332ee38680714292a54e4101a9372ff777
UrvashiHirkane12 <168714099+UrvashiHirkane12@users.noreply.github.com>
2025-08-26 10:34:35 +0530

    39272

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django contrib.auth.models import user
+from django contrib.auth.models import abstractuser
 
 class Feedback(models.Model):
     comment= models.TextField()
@@ -17,3 +18,10 @@ class userprofile(models.model):
 
 def __str__(self):
     return self.user.username
+
+
+class customeruser(abstractuser):
+    name= models.charfield(max_length=100)
+    email=  models.emailfield(unique=True)
+    def __str__(self):
+        return self.username
