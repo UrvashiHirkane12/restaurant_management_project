10c8def915595bcfc039c5e956d61da57528bf2a
UrvashiHirkane12 <168714099+UrvashiHirkane12@users.noreply.github.com>
2025-08-26 10:29:29 +0530

    39268

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django contrib.auth.models import user
 
 class Feedback(models.Model):
     comment= models.TextField()
@@ -6,3 +7,13 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f"Feedback #{self.id} - {self.created_at.strftime('%y-%m-%d')}"
+
+
+class userprofile(models.model):
+    user = models.onetoonefieldO(user, name="profile")        
+    name = models.charfield(max_length=100)
+    email= models.emailfield(unique=True)
+
+
+def __str__(self):
+    return self.user.username
