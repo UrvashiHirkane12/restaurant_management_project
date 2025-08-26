a520f25151f7e1729b02a0c4ccc9d3b0e0a1bb8c
UrvashiHirkane12 <168714099+UrvashiHirkane12@users.noreply.github.com>
2025-08-26 11:05:08 +0530

    39271

--- admin.py
+++ admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import userprofile 
+from .models import Menu, Order
 
 admin.site.register(userprofile)
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "privce")
+    search_field = ("name")
