73db7b03b7f692ed9547fd19b4f91a77d1471da6
UrvashiHirkane12 <168714099+UrvashiHirkane12@users.noreply.github.com>
2025-08-26 16:28:58 +0530

    39367

--- urls.py
+++ urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('feedback/', views.feedback_view, name='feedback'),
     path('feedback/thanks/', views.feedback_thanks, name='feedback_thanks'),
+    path('', views.home, name='home'),
 ]
