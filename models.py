4f4ddcdaf8aa698d14470770f795f696bdcbed11
UrvashiHirkane12 <168714099+UrvashiHirkane12@users.noreply.github.com>
2025-08-30 10:42:40 +0530

    40359

--- models.py
+++ models.py
@@ -67,7 +67,7 @@ class MenuItem(models.Model):
 class Restaurant(models.Model):
     name = ,odels.CharField(max_length=100)
     address = models.TextField()
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.name      
