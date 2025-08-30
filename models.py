c04f476be937800dda1f642d4ee7f45834539791
UrvashiHirkane12 <168714099+UrvashiHirkane12@users.noreply.github.com>
2025-08-30 10:45:55 +0530

    40358

--- models.py
+++ models.py
@@ -68,6 +68,7 @@ class Restaurant(models.Model):
     name = ,odels.CharField(max_length=100)
     address = models.TextField()
     phone_number = models.CharField(max_length=20, blank=True, null=True)
+    logo = models.ImageField(upload_to="restaurant_logos/", blank=True, null=True)
 
     def __str__(self):
         return self.name      
